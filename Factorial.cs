32ab7907f0abb40b8aab0cf05206ccc0d1967601
VishnupriyaP31 <113713344+VishnupriyaP31@users.noreply.github.com>
2022-11-26 06:09:09 +0530

    Create Factorial.cs

---
+++ Factorial.cs
@@ -0,0 +1,13 @@
+using System;
+namespace Practice{
+    class Program{
+        static void Main(string[] args){
+            int n=Convert.ToInt32(Console.ReadLine());
+            int f=1;
+            for(int i=1;i<=n;i++){
+                f=f*i;
+            }
+            Console.WriteLine(f);
+        }
+    }
+}
